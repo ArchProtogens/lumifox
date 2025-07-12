a5c975aca6f2b05cde3864a784530faff7279576
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-07-12 21:26:41 +0200

    feat(examples): add colored move display to print demo
    • Implement `print_move()` function with **ANSI color formatting** for move visualization
    • Display moves in standard algebraic notation (`a1` to `h8` format)
    • Show **first 5 moves** generated by pawn move generator with colored output
    • Add **promotion type display** when applicable for pawn promotion moves
    • Handle `PieceMove::NULL` case with descriptive output
    • Minor license header formatting fixes across source files
    
    Enhances the demo experience by providing visual feedback of generated moves instead of just move counts, making the chess engine output more readable

--- modules/chess/examples/print.rs
+++ modules/chess/examples/print.rs
@@ -1,4 +1,27 @@
-use lumifox_chess::{model::gamedata::GameData, movegen::generate_moves};
+use lumifox_chess::{
+  model::{gamedata::GameData, piecemove::PieceMove},
+  movegen::generate_moves,
+};
+
+fn print_move(piece_move: &PieceMove) {
+  if *piece_move == PieceMove::NULL {
+    println!("NULL move");
+  } else {
+    let from = piece_move.from_square();
+    let to = piece_move.to_square();
+
+    println!(
+      "Move from \x1b[96m{}{}\x1b[0m to \x1b[96m{}{}\x1b[0m",
+      (from / 8 + 1 + b'a') as char,
+      from % 8 + 1,
+      (to / 8 + 1 + b'a') as char,
+      to % 8 + 1
+    );
+    if let Some(promotion) = piece_move.promotion_type() {
+      println!("Promotion to {:?}", promotion);
+    }
+  }
+}
 
 fn main() {
   let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
@@ -19,5 +42,12 @@ fn main() {
 
   let moves = generate_moves(&board.board);
 
-  println!("In total {} can be played with the pawns", moves.1);
+  println!("In total {} can be played with the pawns.\n", moves.1);
+  println!("Such moves include:");
+  for piece_move in moves.0.iter().take(moves.1.min(5)) {
+    print_move(piece_move);
+  }
+  if moves.1 > 5 {
+    println!("... and more moves.");
+  }
 }
--- modules/chess/src/model/gameboard.rs
+++ modules/chess/src/model/gameboard.rs
@@ -9,11 +9,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
 use crate::model::piecemove::PieceMove;
@@ -74,7 +74,7 @@ impl GameBoard {
   pub fn casling_right_white(&self) -> (bool, bool) {
     (
       (self.castling & 0b0001) != 0, // White kingside
-      (self.castling & 0b0010) != 0, // White queenside)
+      (self.castling & 0b0010) != 0, // White queenside
     )
   }
 
