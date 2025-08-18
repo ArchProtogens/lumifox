f8041b1c19f43dc8c20abf038c005af67a83b5c1
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-08-18 17:52:22 +0200

    chore: update license information and remove obsolete test files

--- modules/chess/src/legal/checker.rs
+++ modules/chess/src/legal/checker.rs
@@ -1,6 +1,19 @@
 /*
- * Legal move checker moved into its own module.
- * This is an unoptimised implementation copied from GameBoard logic.
+ * A high-performance chess library licensed under the LGPLv3.
+ * Copyright (C) 2025 Clifton Toaster Reid
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <https://opensource.org/license/lgpl-3-0>.
  */
 
 use crate::legal::attack::is_square_attacked;
